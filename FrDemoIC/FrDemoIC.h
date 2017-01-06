d39ad5c7182646f07127c14f18d50b6e4c6a6ea4
Francois Devove <francoisdevove@MacBook-Pro-de-Francois-2.local>
2017-01-06 10:11:31 +0100

    add existing Xcode project

---
+++ FrDemoIC/FrDemoIC.h
@@ -0,0 +1,19 @@
+//
+//  FrDemoIC.h
+//  FrDemoIC
+//
+//  Created by Francois Devove on 06.01.17.
+//  Copyright © 2017 Francois Devove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrDemoIC.
+FOUNDATION_EXPORT double FrDemoICVersionNumber;
+
+//! Project version string for FrDemoIC.
+FOUNDATION_EXPORT const unsigned char FrDemoICVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrDemoIC/PublicHeader.h>
+
+
